b3076ff067e852b447ac6a304aefb0d3a9eb0137
Lucas <luccastrin@hotmail.com>
2021-03-23 08:16:15 -0300

    layout finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BudgetComponent } from './pages/budget/budget.component';
+import { IncomeComponent } from './pages/budget/income/income.component';
+import { ExpensesComponent } from './pages/budget/expenses/expenses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BudgetComponent,
+    IncomeComponent,
+    ExpensesComponent
   ],
   imports: [
     BrowserModule,
